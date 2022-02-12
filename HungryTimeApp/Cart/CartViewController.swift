edba140e7513f842dd4867a02072c0617ea60216
Prashant Ghimire <prashant.ghimire@gmail.com>
2022-02-12 07:56:03 +0545

    folder arrangement

---
+++ HungryTimeApp/Cart/CartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CartViewController.swift
+//  HungryTimeApp
+//
+//  Created by Prashant Ghimire on 2/12/22.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+// MARK: - View Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ HungryTimeApp/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  HungryTimeApp
+//
+//  Created by Prashant Ghimire on 2/12/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+// MARK: - View Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
